f02bd51ff906854b1cad61937fbdde7292255d76
linkmiguel1 <miguelmedinadominguez@gmail.com>
2025-03-03 13:40:12 +0100

    Conexion funcional

--- python-app/main.py
+++ python-app/main.py
@@ -1,16 +1,24 @@
 import requests
+import time
+import os
 
 def main():
-    # Nota: Dentro del contenedor, se puede acceder al servicio 'grobid' por su nombre de servicio
-    url = "http://grobid:8070"
-    try:
-        response = requests.get(url)
-        if response.status_code == 200:
-            print("¡La página de bienvenida de GROBID se ha accedido correctamente!")
-        else:
-            print("Error al acceder a la página de bienvenida. Código de estado:", response.status_code)
-    except Exception as e:
-        print("Excepción al intentar acceder a GROBID:", e)
+    url = os.environ.get("GROBID_URL", "http://grobid:8070")
+    retries = 10  # número de intentos
+    delay = 10    # segundos entre cada intento
+
+    for i in range(retries):
+        try:
+            response = requests.get(url)
+            if response.status_code == 200:
+                print("¡La página de bienvenida de GROBID se ha accedido correctamente!")
+                return
+            else:
+                print(f"Intento {i+1}: Error al acceder, código de estado: {response.status_code}")
+        except Exception as e:
+            print(f"Intento {i+1}: Excepción al intentar acceder a GROBID: {e}")
+        time.sleep(delay)
+    print("No se pudo acceder a GROBID después de varios intentos.")
 
 if __name__ == "__main__":
     main()
